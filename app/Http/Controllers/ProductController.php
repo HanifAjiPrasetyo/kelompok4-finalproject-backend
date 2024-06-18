5b70417482ae64d10ac55a31949af6b11f8d3cb2
HanifAjiPrasetyo <hanifaji007@gmail.com>
2024-06-18 20:51:11 +0700

    add, delete, search product admin

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Resources\ProductResource;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -33,18 +34,39 @@ class ProductController extends Controller
     {
     }
 
+    public function getCategories()
+    {
+        return response()->json(Category::all());
+    }
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreProductRequest $request)
     {
-        $validator = Validator::make(request()->all(), $request);
 
-        if ($validator->fails()) {
-            return response()->json($validator->messages(), 422);
+        if (auth()->user()->is_admin) {
+            $product = Product::create($request->validated());
+
+            $imageName = $product->slug . '.' . explode("/", $request->file('image')->getClientMimeType())[1];
+            $request->file('image')->storeAs('images/products', $imageName);
+
+            $imagePath = 'http://localhost:8000/storage/images/products/' . $imageName;
+            $product->image = $imagePath;
+            $product->save();
+
+            if ($product) {
+                return response()->json([
+                    'message' => 'Product created successfully',
+                    'product' => $product,
+                ]);
+            }
         }
 
-        return response()->json($request->validated());
+        return response()->json([
+            'success' => false,
+            'message' => "Unauthorized"
+        ], 401);
     }
 
     /**
@@ -78,8 +100,23 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($productId)
     {
-        //
+        $product = Product::find($productId);
+
+        if (auth()->user()->is_admin) {
+            if ($product->cartItems()->count() > 0 || $product->orderItems()->count() > 0) {
+                return response()->json([
+                    'message' => 'Cannot delete this product, it has related data in the cart or order.',
+                ], 409);
+            } else {
+                $product->delete();
+                Storage::delete($product->image);
+
+                return response()->json([
+                    'message' => "Product deleted successfully",
+                ]);
+            }
+        }
     }
 }
